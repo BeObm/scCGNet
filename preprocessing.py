6e06a4e7e246c06f68f8b6120937dfb60f16f02d
BABATOUNDE MOCTARD <54064640+BeObm@users.noreply.github.com>
2026-05-01 10:11:03 -0400

    update

--- preprocessing.py
+++ preprocessing.py
@@ -192,7 +192,7 @@ def get_device():
         device = torch.device("cpu")
         print("Using CPU")
 
-    return device # Just
+    return torch.device("cpu") # Just
 
 
 def clustering_metrics(y_true, y_pred):
